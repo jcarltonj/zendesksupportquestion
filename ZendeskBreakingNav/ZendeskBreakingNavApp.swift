84757a56676e2a77707bcf5d028ac8611103346f
Carlton Jester <jcarltonj@gmail.com>
2022-08-09 10:42:22 -0500

    Initial Commit

---
+++ ZendeskBreakingNav/ZendeskBreakingNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZendeskBreakingNavApp.swift
+//  ZendeskBreakingNav
+//
+//  Created by Carlton Jester on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ZendeskBreakingNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
